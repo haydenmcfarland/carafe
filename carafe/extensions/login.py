192fee5e5a621ae7cf172919ec54efc8b4c30b52
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-14 17:50:06 -0800

    refactor login.py

--- carafe/extensions/login.py
+++ carafe/extensions/login.py
@@ -1,5 +1,8 @@
+""" Carafe Login Extension """
+
 from flask_login import AnonymousUserMixin
 
 
 class AnonymousUser(AnonymousUserMixin):
+    """ Carafe Anonymous User Mixin """
     is_admin = False
