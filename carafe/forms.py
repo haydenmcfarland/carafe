a1b070c579788b48a890cc6bdfaf3cdbfc47d9e3
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-14 16:03:29 -0800

    autopep and flake

--- carafe/forms.py
+++ carafe/forms.py
@@ -1,36 +1,70 @@
-from wtforms import StringField, Form, PasswordField, TextAreaField, BooleanField, validators
+from wtforms import (
+    StringField, Form, PasswordField, TextAreaField, BooleanField, validators
+)
 from wtforms.validators import Length, DataRequired, EqualTo
 from carafe import constants
 
 
 class SignupForm(Form):
-    username = StringField('Username', [Length(min=constants.USERNAME_MIN, max=constants.USERNAME_LIMIT)])
-    email = StringField('Email Address', [Length(min=constants.EMAIL_MIN, max=constants.EMAIL_LIMIT)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match.')])
+    username = StringField(
+        'Username', [
+            Length(
+                min=constants.USERNAME_MIN, max=constants.USERNAME_LIMIT)])
+    email = StringField(
+        'Email Address', [
+            Length(
+                min=constants.EMAIL_MIN, max=constants.EMAIL_LIMIT)])
+    password = PasswordField(
+        'Password', [
+            validators.DataRequired(), validators.EqualTo(
+                'confirm', message='Passwords must match.')])
     confirm = PasswordField('Repeat Password')
 
 
 class LoginForm(Form):
-    username = StringField('Username', [Length(min=constants.USERNAME_MIN, max=constants.USERNAME_LIMIT)])
-    password = PasswordField('Password', [DataRequired(), EqualTo('confirm', message='Passwords must match.')])
+    username = StringField(
+        'Username', [
+            Length(
+                min=constants.USERNAME_MIN, max=constants.USERNAME_LIMIT)])
+    password = PasswordField(
+        'Password', [
+            DataRequired(), EqualTo(
+                'confirm', message='Passwords must match.')])
     confirm = PasswordField('Repeat Password')
 
 
 class BoardForm(Form):
-    name = StringField('Board Name', [Length(min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
-    desc = TextAreaField('Description', [Length(min=constants.DESC_MIN, max=constants.DESC_LIMIT)])
+    name = StringField(
+        'Board Name', [
+            Length(
+                min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
+    desc = TextAreaField(
+        'Description', [
+            Length(
+                min=constants.DESC_MIN, max=constants.DESC_LIMIT)])
 
 
 class PostForm(Form):
-    name = StringField('Post Title', [Length(min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
-    desc = TextAreaField('Content', [Length(min=constants.TEXT_MIN, max=constants.TEXT_LIMIT)])
+    name = StringField(
+        'Post Title', [
+            Length(
+                min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
+    desc = TextAreaField(
+        'Content', [
+            Length(
+                min=constants.TEXT_MIN, max=constants.TEXT_LIMIT)])
 
 
 class CommentForm(Form):
-    text = TextAreaField('Leave a comment:', [Length(min=constants.TEXT_MIN, max=constants.TEXT_LIMIT)])
+    text = TextAreaField(
+        'Leave a comment:', [
+            Length(
+                min=constants.TEXT_MIN, max=constants.TEXT_LIMIT)])
 
 
 class ConfigForm(Form):
-    name = StringField('Carafe Board Name', [Length(min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
+    name = StringField(
+        'Carafe Board Name', [
+            Length(
+                min=constants.NAME_MIN, max=constants.NAME_LIMIT)])
     enable_registration = BooleanField('Enable registration')
-
