de3b6d2c13ea321294c04ba7813385b6c8c5437d
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-14 17:24:36 -0800

    refactor app.py

--- carafe/app.py
+++ carafe/app.py
@@ -1,9 +1,12 @@
+""" Carafe Application """
 from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_login import (
     LoginManager, current_user, login_user, login_required, logout_user
 )
 from htmlmin.main import minify
 from passlib.hash import sha512_crypt as sha
+from sqlalchemy import text
+
 from carafe.config import load_config
 from carafe.extensions.login import AnonymousUser
 from carafe.database.model import Board, Post, Comment, User, db
@@ -12,27 +15,32 @@ from carafe.forms import (
 )
 from carafe.rest.api import api
 from carafe import constants
-from sqlalchemy import text
 
 # APP INIT
-app = Flask(__name__)
-app.register_blueprint(api)
-load_config(app)
-db.init_app(app)
-db.app = app
-login_manager = LoginManager()
-login_manager.anonymous_user = AnonymousUser
-login_manager.init_app(app)
+APP = Flask(__name__)
+APP.register_blueprint(api)
+load_config(APP)
+db.init_app(APP)
+db.app = APP
+LOGIN_MANAGER = LoginManager()
+LOGIN_MANAGER.anonymous_user = AnonymousUser
+LOGIN_MANAGER.init_app(APP)
 
 # ERROR HANDLER
-@app.errorhandler(404)
-def page_not_found(e):
+@APP.errorhandler(404)
+def page_not_found(_error):
+    """
+    error handler for 404
+    """
     return render_template('404.html'), 404
 
 
 # HTML Response Decorator to Minimize HTML
-@app.after_request
+@APP.after_request
 def response_minify(response):
+    """
+    minifies html response
+    """
     if response.content_type == u'text/html; charset=utf-8':
         response.set_data(minify(response.get_data(as_text=True)))
         return response
@@ -40,12 +48,15 @@ def response_minify(response):
 
 
 # Login Manager Decorators
-@login_manager.unauthorized_handler
+@LOGIN_MANAGER.unauthorized_handler
 def unauthorized_callback():
+    """
+    unauthorized callbacks redirect
+    """
     return redirect('/login?next=' + request.path)
 
 
-@login_manager.user_loader
+@LOGIN_MANAGER.user_loader
 def load_user(uid):
     """
     callback that is used to reload user object from the User uid
@@ -53,23 +64,21 @@ def load_user(uid):
     return User.query.get(uid)
 
 
-# Context Processors for Jinja2 Template Access
-def context_config():
-    return dict(config=app.config)
-
-
 # Database Helper function to Initialize Database Tables
 def create_database_tables():
     """
     initializes database in the proper application context
     """
-    with app.app_context():
+    with APP.app_context():
         db.create_all()
 
 
 # Routes
-@app.route('/', methods=constants.METHODS)
+@APP.route('/', methods=constants.METHODS)
 def index():
+    """
+    application index route
+    """
     return render_template(
         'index.html',
         boards=Board.query.filter_by(
@@ -77,9 +86,12 @@ def index():
         form=BoardForm())
 
 
-@app.route('/admin/panel', methods=constants.METHODS)
+@APP.route('/admin/panel', methods=constants.METHODS)
 @login_required
 def panel():
+    """
+    admin panel route
+    """
     if current_user.is_admin:
         return render_template(
             'panel.html',
@@ -87,11 +99,10 @@ def panel():
             Post=Post,
             Comment=Comment,
             Board=Board)
-    else:
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 
-@app.route('/board/<bid>')
+@APP.route('/board/<bid>')
 def board(bid):
     """
     view that displays the post of the board specified by the provided bid
@@ -99,42 +110,48 @@ def board(bid):
     form = PostForm(request.form)
     posts = Post.query.filter_by(
         bid=bid, deleted=False).outerjoin(Comment).order_by(
-        text("comment.date desc"))
-    b = Board.query.get(bid)
-    return render_template('posts.html', posts=posts, b=b, pform=form)
+            text("comment.date desc"))
+    brd = Board.query.get(bid)
+    return render_template('posts.html', posts=posts, b=brd, pform=form)
 
 
-@app.route('/board/<bid>/post/<pid>', methods=constants.METHODS)
+@APP.route('/board/<bid>/post/<pid>', methods=constants.METHODS)
 def post(bid, pid):
-    p = Post.query.get(pid)
-    if p.deleted:
+    """
+    post route
+    """
+    pst = Post.query.get(pid)
+    if pst.deleted:
         flash('The post you are trying to access has been deleted.')
         return redirect(url_for('board', bid=bid))
     comments = Comment.query.filter_by(pid=pid).order_by(text("date asc"))
     return render_template(
         'post.html',
-        p=p,
+        p=pst,
         bid=bid,
         comments=comments,
         pform=PostForm(),
         cform=CommentForm())
 
 
-@app.route('/signup', methods=constants.METHODS)
+@APP.route('/signup', methods=constants.METHODS)
 def sign_up():
-    if not current_user.is_admin and not app.config['REGISTRATION_FLAG']:
+    """
+    user registration route
+    """
+    if not current_user.is_admin and not APP.config['REGISTRATION_FLAG']:
         flash('Sorry, user registration is disabled.')
         return redirect(url_for('index'))
 
-    elif current_user.is_authenticated:
+    if current_user.is_authenticated:
         flash('You are already logged in!')
         return redirect(url_for('index'))
 
     form = SignupForm(request.form)
     if request.method == 'POST' and form.validate():
-        for u in User.query.all():
-            if (u.username == form.username.data.lower() or
-                    u.email == form.email.data.lower()):
+        for user in User.query.all():
+            if (user.username == form.username.data.lower() or
+                    user.email == form.email.data.lower()):
                 flash('Username or email is already taken.')
                 return render_template('signup.html', form=form)
         user = User(
@@ -149,7 +166,7 @@ def sign_up():
     return render_template('signup.html', form=form)
 
 
-@app.route("/login", methods=constants.METHODS)
+@APP.route("/login", methods=constants.METHODS)
 def login():
     """
     logs in user if proper parameters are entered and creates a session for
@@ -166,21 +183,22 @@ def login():
             login_user(user)
             flash('You are logged in as {}!'.format(user.username))
             return redirect(url_for('index'))
-        else:
-            flash('User either does not exist or password is invalid.')
+        flash('User either does not exist or password is invalid.')
     return render_template('login.html', form=form)
 
 
-@app.route("/logout")
+@APP.route("/logout")
 @login_required
 def logout():
-    """ logs out user and redirects to front page """
+    """
+    logs out user and redirects to front page
+    """
     logout_user()
     flash('Successfully logged out.')
     return redirect(request.referrer)
 
 
-@app.route('/board/create', methods=constants.METHODS)
+@APP.route('/board/create', methods=constants.METHODS)
 def create_board():
     """
     creates board if form parameters meet requirements
@@ -193,7 +211,7 @@ def create_board():
             db.session.commit()
             flash('Board ({}) successfully created!'.format(form.name.data))
         else:
-            flash('Duplicate board detected.'.format(form.name.data))
+            flash('Duplicate board detected.')
     return render_template(
         'index.html',
         boards=Board.query.filter_by(
@@ -201,18 +219,18 @@ def create_board():
         form=form)
 
 
-@app.route('/board/edit/<bid>', methods=constants.METHODS)
+@APP.route('/board/edit/<bid>', methods=constants.METHODS)
 def edit_board(bid):
     """
     allows the editing of a board with the provided bid
     """
     form = BoardForm(request.form)
-    b = Board.query.get(bid)
+    brd = Board.query.get(bid)
     if current_user.is_admin and request.method == 'POST':
         if form.validate():
-            if b.name != form.name.data or b.desc != form.desc.data:
-                b.name = form.name.data
-                b.desc = form.desc.data
+            if brd.name != form.name.data or brd.desc != form.desc.data:
+                brd.name = form.name.data
+                brd.desc = form.desc.data
                 db.session.commit()
                 flash('Board ({}) successfully edited!'.format(form.name.data))
         else:
@@ -220,7 +238,7 @@ def edit_board(bid):
     return redirect(request.referrer)
 
 
-@app.route('/board/<bid>/delete')
+@APP.route('/board/<bid>/delete')
 @login_required
 def delete_board(bid):
     """
@@ -234,7 +252,7 @@ def delete_board(bid):
 
 
 # POST VIEWS
-@app.route('/board/<bid>/post/create', methods=constants.METHODS)
+@APP.route('/board/<bid>/post/create', methods=constants.METHODS)
 @login_required
 def create_post(bid):
     """
@@ -257,21 +275,21 @@ def create_post(bid):
     return redirect(request.referrer)
 
 
-@app.route('/board/<bid>/post/<pid>/edit', methods=constants.METHODS)
+@APP.route('/board/<bid>/post/<pid>/edit', methods=constants.METHODS)
 @login_required
-def edit_post(bid, pid):
+def edit_post(_bid, pid):
     """
     allows the editing of a post as long as the active user is the post
     creator or admin
     """
-    p = Post.query.get(pid)
+    pst = Post.query.get(pid)
     form = PostForm(request.form)
-    if request.method == 'POST' and current_user.uid == p.uid:
+    if request.method == 'POST' and current_user.uid == pst.uid:
         if form.validate():
-            if p.name != form.name.data or p.text != form.desc.data:
-                og_name = p.name
-                p.name = form.name.data
-                p.text = form.desc.data
+            if pst.name != form.name.data or pst.text != form.desc.data:
+                og_name = pst.name
+                pst.name = form.name.data
+                pst.text = form.desc.data
                 db.session.commit()
                 flash('Post ({}) successfully edited!'.format(og_name))
         else:
@@ -279,9 +297,9 @@ def edit_post(bid, pid):
     return redirect(request.referrer)
 
 
-@app.route('/board/<bid>/post/<pid>/delete')
+@APP.route('/board/<bid>/post/<pid>/delete')
 @login_required
-def delete_post(bid, pid):
+def delete_post(_bid, pid):
     """
     allows for the deletion of a post as long as the active user is the post
     creator or admin
@@ -293,9 +311,9 @@ def delete_post(bid, pid):
 
 
 # COMMENT VIEWS
-@app.route('/board/<bid>/post/<pid>/comment', methods=constants.METHODS)
+@APP.route('/board/<bid>/post/<pid>/comment', methods=constants.METHODS)
 @login_required
-def create_comment(bid, pid):
+def create_comment(_bid, pid):
     """
     allows for the creation of a comment by an active user
     """
@@ -310,21 +328,21 @@ def create_comment(bid, pid):
     return redirect(request.referrer)
 
 
-@app.route(
+@APP.route(
     '/board/<bid>/post/<pid>/comment/<cid>/edit',
     methods=constants.METHODS)
 @login_required
-def edit_comment(bid, pid, cid):
+def edit_comment(_bid, _pid, cid):
     """
     allows the editing of a post as long as the active user is the post creator
     or admin
     """
-    c = Comment.query.get(cid)
+    comment = Comment.query.get(cid)
     form = CommentForm(request.form)
-    if request.method == 'POST' and current_user.uid == c.uid:
+    if request.method == 'POST' and current_user.uid == comment.uid:
         if form.validate():
-            if c.text != form.text.data:
-                c.text = form.text.data
+            if comment.text != form.text.data:
+                comment.text = form.text.data
                 db.session.commit()
                 flash('Comment successfully edited!')
         else:
@@ -332,9 +350,9 @@ def edit_comment(bid, pid, cid):
     return redirect(request.referrer)
 
 
-@app.route('/board/<bid>/post/<pid>/comment/<cid>/delete')
+@APP.route('/board/<bid>/post/<pid>/comment/<cid>/delete')
 @login_required
-def delete_comment(bid, pid, cid):
+def delete_comment(_bid, pid, cid):
     """
     allows for a comment to be 'deleted' by the comment creator or admin
     """
@@ -345,8 +363,8 @@ def delete_comment(bid, pid, cid):
     return redirect(request.referrer)
 
 
-@app.route('/board/<bid>/post/<pid>/comment/<cid>/revive')
-def revive_comment(bid, pid, cid):
+@APP.route('/board/<bid>/post/<pid>/comment/<cid>/revive')
+def revive_comment(_bid, pid, cid):
     """
     allows for a 'deleted' comment to be 'revived' by an admin
     """
@@ -357,9 +375,12 @@ def revive_comment(bid, pid, cid):
     return redirect(request.referrer)
 
 
-@app.route('/admin/board/<bid>/erase')
+@APP.route('/admin/board/<bid>/erase')
 @login_required
 def erase_board(bid):
+    """
+    admin "erase" board route
+    """
     if current_user.is_admin:
         db.session.delete(Board.query.get(bid))
         db.session.commit()
@@ -371,9 +392,12 @@ def erase_board(bid):
     return redirect(request.referrer)
 
 
-@app.route('/admin/board/<bid>/revive')
+@APP.route('/admin/board/<bid>/revive')
 @login_required
 def revive_board(bid):
+    """
+    admin revive board route
+    """
     if current_user.is_admin:
         Board.query.get(bid).deleted = False
         db.session.commit()
