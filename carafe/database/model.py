7acd50b2d7c47a35fa41dd71220cf9d6901ed63d
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-14 17:48:17 -0800

    pylint and model changes

--- carafe/database/model.py
+++ carafe/database/model.py
@@ -1,22 +1,28 @@
-from flask_sqlalchemy import SQLAlchemy
+""" Carafe Models """
+
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from carafe.forms import BoardForm, PostForm, CommentForm
 from carafe.database.base import CarafeObj, UserContent
 from carafe import constants
-from sqlalchemy import text
-db = SQLAlchemy()
+
+DB = SQLAlchemy()
 
 
-class User(db.Model, CarafeObj):
-    uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(constants.USERNAME_LIMIT), unique=True)
-    email = db.Column(db.String(constants.EMAIL_LIMIT), unique=True)
-    password = db.Column(db.String(128))
-    is_active = db.Column(db.Boolean)
-    is_anonymous = db.Column(db.Boolean)
-    is_authenticated = db.Column(db.Boolean)
-    is_confirmed = db.Column(db.Boolean)
-    is_admin = db.Column(db.Boolean)
+class User(DB.Model, CarafeObj):
+    """
+    Carafe User class that describes registered user
+    """
+    uid = DB.Column(DB.Integer, primary_key=True)
+    username = DB.Column(DB.String(constants.USERNAME_LIMIT), unique=True)
+    email = DB.Column(DB.String(constants.EMAIL_LIMIT), unique=True)
+    password = DB.Column(DB.String(128))
+    is_active = DB.Column(DB.Boolean)
+    is_anonymous = DB.Column(DB.Boolean)
+    is_authenticated = DB.Column(DB.Boolean)
+    is_confirmed = DB.Column(DB.Boolean)
+    is_admin = DB.Column(DB.Boolean)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -28,14 +34,20 @@ class User(db.Model, CarafeObj):
         self.is_confirmed = False
 
     def get_id(self):
+        """
+        retrieves the user's id
+        """
         return self.uid
 
 
-class Board(db.Model, CarafeObj):
-    bid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(constants.NAME_LIMIT), unique=True)
-    desc = db.Column(db.String(constants.DESC_LIMIT))
-    deleted = db.Column(db.Boolean)
+class Board(DB.Model, CarafeObj):
+    """
+    Carafe Board class that describes forum boards
+    """
+    bid = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(constants.NAME_LIMIT), unique=True)
+    desc = DB.Column(DB.String(constants.DESC_LIMIT))
+    deleted = DB.Column(DB.Boolean)
 
     def __init__(self, name, desc):
         self.name = name
@@ -43,94 +55,131 @@ class Board(db.Model, CarafeObj):
         self.deleted = False
 
     def get_recent_post(self):
+        """
+        gets most recent board post
+        """
         return Post.query.filter_by(
             bid=self.id).order_by(
-            text('date desc')).first()
+                text('date desc')).first()
 
     def get_post_count(self):
+        """
+        gets the number of posts on a board
+        """
         return Post.query.filter_by(bid=self.id).count()
 
     def get_edit_form(self):
+        """
+        gets the edit for for a board
+        """
         form = BoardForm()
         form.name.data = self.name
         form.desc.data = self.desc
         return form
 
     def get_username(self):
+        """
+        board method to get username
+        """
         return User.query.get(self.uid).username
 
 
-class Post(db.Model, CarafeObj, UserContent):
-    pid = db.Column(db.Integer, primary_key=True)
-    bid = db.Column(db.Integer, db.ForeignKey(Board.bid, ondelete='CASCADE'))
-    uid = db.Column(db.Integer, db.ForeignKey(User.uid, ondelete='CASCADE'))
-    date = db.Column(db.DateTime, index=True)
-    date_edited = db.Column(db.DateTime, index=True)
-    name = db.Column(db.String(constants.NAME_LIMIT))
-    text = db.Column(db.String(constants.TEXT_LIMIT))
-    deleted = db.Column(db.Boolean)
+class Post(DB.Model, CarafeObj, UserContent):
+    """
+    Carafe Post class that describes board posts
+    """
+    pid = DB.Column(DB.Integer, primary_key=True)
+    bid = DB.Column(DB.Integer, DB.ForeignKey(Board.bid, ondelete='CASCADE'))
+    uid = DB.Column(DB.Integer, DB.ForeignKey(User.uid, ondelete='CASCADE'))
+    date = DB.Column(DB.DateTime, index=True)
+    date_edited = DB.Column(DB.DateTime, index=True)
+    name = DB.Column(DB.String(constants.NAME_LIMIT))
+    text = DB.Column(DB.String(constants.TEXT_LIMIT))
+    deleted = DB.Column(DB.Boolean)
 
-    def __init__(self, bid, uid, name, text):
+    def __init__(self, bid, uid, name, txt):
         self.bid = bid
         self.uid = uid
         self.date = datetime.now()
         self.date_edited = self.date
         self.name = name
-        self.text = text
+        self.text = txt
         self.deleted = False
 
     def get_comment_count(self):
+        """
+        get total comment count on post
+        """
         return Comment.query.filter_by(pid=self.id).count()
 
     def get_latest_comment_info(self):
+        """
+        get the latest comment info on the post
+        """
         comment = Comment.query.filter_by(
             pid=self.id).order_by(
-            text("date desc")).first()
+                text("date desc")).first()
         if comment:
             return comment.get_date_str()
-        else:
-            return 'None'
+        return 'None'
 
     def recent_date(self):
+        """
+        get most recent comment date or post date
+        """
         comment = Comment.query.filter_by(
             pid=self.id).order_by(
-            text("date desc")).first()
+                text("date desc")).first()
         if comment:
             return comment.date
-        else:
-            return self.date
+        return self.date
 
     def get_edit_form(self):
+        """
+        gets post edit form
+        """
         form = PostForm()
         form.name.data = self.name
         form.desc.data = self.text
         return form
 
     def get_username(self):
+        """
+        post helper to get username by id
+        """
         return User.query.get(self.uid).username
 
 
-class Comment(db.Model, CarafeObj, UserContent):
-    cid = db.Column(db.Integer, primary_key=True)
-    pid = db.Column(db.Integer, db.ForeignKey(Post.pid, ondelete='CASCADE'))
-    uid = db.Column(db.Integer, db.ForeignKey(User.uid, ondelete='CASCADE'))
-    date = db.Column(db.DateTime, index=True)
-    date_edited = db.Column(db.DateTime, index=True)
-    text = db.Column(db.String(constants.TEXT_LIMIT))
-    deleted = db.Column(db.Boolean)
+class Comment(DB.Model, CarafeObj, UserContent):
+    """
+    Carafe Comment class that describes post comments
+    """
+    cid = DB.Column(DB.Integer, primary_key=True)
+    pid = DB.Column(DB.Integer, DB.ForeignKey(Post.pid, ondelete='CASCADE'))
+    uid = DB.Column(DB.Integer, DB.ForeignKey(User.uid, ondelete='CASCADE'))
+    date = DB.Column(DB.DateTime, index=True)
+    date_edited = DB.Column(DB.DateTime, index=True)
+    text = DB.Column(DB.String(constants.TEXT_LIMIT))
+    deleted = DB.Column(DB.Boolean)
 
-    def __init__(self, pid, uid, text):
+    def __init__(self, pid, uid, txt):
         self.pid = pid
         self.uid = uid
-        self.text = text
+        self.text = txt
         self.date = datetime.now()
         self.date_edited = self.date
         self.deleted = False
 
     def get_edit_form(self):
+        """
+        get edit form of comment
+        """
         form = CommentForm()
         form.text.data = self.text
         return form
 
     def get_username(self):
+        """
+        comment method to get username
+        """
         return User.query.get(self.uid).username
