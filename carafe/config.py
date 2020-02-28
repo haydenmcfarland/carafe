0a335430314d24cdbd8adccf32d6499f92efe482
Hayden McFarland <mcfarlandsms@gmail.com>
2020-02-27 21:21:04 -0800

    config.py refactor and remove bad username/email check

--- carafe/config.py
+++ carafe/config.py
@@ -1,4 +1,5 @@
 """ Carafe Configration """
+import os
 from os import environ
 
 
@@ -6,11 +7,12 @@ def load_config(app):
     """
     loads application configuration variables
     """
-    app.config['DEBUG'] = True
+
+    app.config['DEBUG'] = os.getenv(
+        'FLASK_ENV', 'development') == 'development'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['NAME'] = 'Carafe'
-    app.config['LOCAL'] = False
-    if app.config['LOCAL']:
+    app.config['NAME'] = os.getenv('CARAFE_APPLICATION_NAME', 'Carafe')
+    if app.config['DEBUG']:
         app.config['SECRET_KEY'] = 'Not so secret key'
         app.config['SQLALCHEMY_DATABASE_URI'] = (
             'postgresql://postgres:password@localhost/postgres'
@@ -18,9 +20,10 @@ def load_config(app):
         app.config['THREADED'] = False
     else:
         app.config['THREADED'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
+        app.config['SQLALCHEMY_DATABASE_URI'] = environ['CARAFE_DATABASE_URL']
         app.config['SECRET_KEY'] = environ['SECRET_KEY']
 
-    app.config['PORT'] = 8000
-    app.config['SETUP'] = False
-    app.config['REGISTRATION_FLAG'] = environ['REGISTRATION_FLAG'] == 'true'
+    app.config['PORT'] = os.getenv('CARAFE_PORT', 8000)
+    app.config['REGISTRATION_FLAG'] = os.getenv(
+        'CARAFE_REGISTRATION') == 'true'
+    app.config['HOST'] = os.getenv('CARAFE_HOST', '0.0.0.0')
